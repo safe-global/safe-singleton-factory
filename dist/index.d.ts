7d78fa86edec0ad9907650d5f78885f69a1787ff
Richard Meissner <richard@gnosis.io>
2021-09-28 11:56:28 +0200

    Remove dist

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-export interface SingletonFactoryInfo {
-    gasPrice: number;
-    gasLimit: number;
-    signerAddress: string;
-    transaction: string;
-    address: string;
-}
-export declare const getSingletonFactoryInfo: (chainId: number) => SingletonFactoryInfo | undefined;
-//# sourceMappingURL=index.d.ts.map
