4b50f124e7aae7de1fa8087806a296a07f760d0f
Richard Meissner <msc.meissner@gmail.com>
2022-04-25 22:26:31 +0200

    Add status script; Add meter network; Add verse network

---
+++ scripts/status.ts
@@ -0,0 +1,26 @@
+import { promises as filesystem } from 'fs'
+import * as path from 'path'
+import { ethers } from 'ethers'
+import dotenv from "dotenv";
+
+dotenv.config()
+
+async function doStuff() {
+	const rpcUrl = process.env.RPC
+	const provider = new ethers.providers.JsonRpcProvider(rpcUrl)
+	const chainId = (await provider.getNetwork()).chainId
+	console.log({chainId})
+	const filePath = path.join(__dirname, "..", "artifacts", `${chainId}`, "deployment.json")
+	const deployment = JSON.parse((await filesystem.readFile(filePath)).toString())
+	const address = deployment.address
+	console.log({address})
+	const code = await provider.getCode(address)
+	console.log({code})
+}
+
+doStuff().then(() => {
+	process.exit(0)
+}).catch(error => {
+	console.error(error)
+	process.exit(1)
+})
