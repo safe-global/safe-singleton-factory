2ffad20860c9e8448fec80b5e411a7f9e5b0e99e
Nicholas Rodrigues Lordello <nick@safe.global>
2024-05-03 13:27:54 +0200

    Adjust GitHub Workflow to Use Labels (#448)
    Fixes #438
    
    This PR uses two new labels to signal statuses for new chain issues in
    the repository:
    
    - `new-chain` label is used when a "new chain" issue is created, this is
    used for filtering by the GH workflow to not spam unrelated issues such
    as the one fixed by this PR with failure messages
    - `ready-to-deploy` label which is used to indicate that the GH workflow
    checks on the new chain succeeded, and that it is ready to deploy. This
    is a helpful precursor to facilitating other helper scripts that run the
    `estimate` and `compile` NPM scripts for all ready chains

--- .github/scripts/validate_new_chain_request.sh
+++ .github/scripts/validate_new_chain_request.sh
@@ -61,16 +61,22 @@ trim() {
     echo "$var"
 }
 
+report_error() {
+    echo "COMMENT_OUTPUT=$1" >> $GITHUB_ENV
+    echo "LABEL_OPERATION=--remove-label" >> $GITHUB_ENV
+    exit 1
+}
+
 rpc_url=$(echo "$ISSUE_BODY" | egrep -o 'https?://[^ ]+' -m 1 | head -1)
 
 if [ -z "$rpc_url" ]; then
-    echo "COMMENT_OUTPUT=$ERROR_MSG_RPC_FAILURE" >> $GITHUB_ENV
-    exit 1
+    echo "Failed to parse RPC URL from issue body"
+    report_error "$ERROR_MSG_RPC_FAILURE"
 fi
 
 rpc_url="$(trim "$rpc_url")"
 echo "Extracted RPC url: $rpc_url. Trying to get the chain id..."
-response=$(curl "$rpc_url" --location --header \
+response=$(curl -s "$rpc_url" --location --header \
     'Content-Type: application/json' --data '{
                           "jsonrpc": "2.0",
                           "method": "eth_chainId",
@@ -83,12 +89,11 @@ if jq -e . >/dev/null 2>&1 <<< "$response"; then
   chain_id=$(( $(echo "$response" | jq -r '.result') ))
 else
     echo "Failed to parse JSON, or got false/null"
-    echo "COMMENT_OUTPUT=$ERROR_MSG_RPC_FAILURE" >> $GITHUB_ENV
-    exit 1
+    report_error "$ERROR_MSG_RPC_FAILURE"
 fi
 
 
-factory_code=$(curl "$rpc_url" --location --header \
+factory_code=$(curl -s "$rpc_url" --location --header \
   'Content-Type: application/json' --data '{
                 "jsonrpc": "2.0",
                 "method": "eth_getCode",
@@ -100,20 +105,18 @@ if jq -e . >/dev/null 2>&1 <<< "$factory_code"; then
   factory_code=$(jq -r '.result' <<< "$factory_code")
 
   if [ "$factory_code" != "0x" ] && [ "$factory_code" != "" ]; then
-    echo "COMMENT_OUTPUT=$FACTORY_ALREADY_DEPLOYED_ERR_MSG" >> $GITHUB_ENV
-    exit 1
+    report_error "$FACTORY_ALREADY_DEPLOYED_ERR_MSG"
   fi
 fi
 
 echo "$chain_id from the RPC URL is valid. Checking the chain in chainlist..."
 chainlist_url="https://raw.githubusercontent.com/ethereum-lists/chains/master/_data/chains/eip155-$chain_id.json"
 
-chainlist_status_code=$(trim "$(curl -LI "$chainlist_url" -o /dev/null -w '%{http_code}\n' -s)")
+chainlist_status_code=$(trim "$(curl -sLI "$chainlist_url" -o /dev/null -w '%{http_code}\n' -s)")
 
 if [ "$chainlist_status_code" == "404" ]; then
   echo "Chain $chain_id is not in chainlist."
-  echo "COMMENT_OUTPUT=$ERROR_MSG_CHAINLIST_FAILURE" >> $GITHUB_ENV
-  exit 1
+  report_error "$ERROR_MSG_CHAINLIST_FAILURE"
 fi
 
 json_request='[
@@ -136,7 +139,7 @@ json_request='[
     }
 ]'
 
-response=$(curl "$rpc_url" --location --header 'Content-Type: application/json' --data "$json_request")
+response=$(curl -s "$rpc_url" --location --header 'Content-Type: application/json' --data "$json_request")
 
 if jq -e . >/dev/null 2>&1 <<< "$response"; then
   gas_price=$(jq -r '.[0].result' <<< "$response")
@@ -145,8 +148,7 @@ if jq -e . >/dev/null 2>&1 <<< "$response"; then
   echo "Estimated deployment gas limit: $gas_limit"
 
   if [[ "$gas_price" == "null" || "$gas_limit" == "null" ]]; then
-    echo "COMMENT_OUTPUT=$ERROR_MSG_PREFUND_CHECK" >> $GITHUB_ENV
-    exit 1
+    report_error "$ERROR_MSG_PREFUND_CHECK"
   fi
 
   # We multiply the gas limit by 1.4, just like the deployment script does it
@@ -155,7 +157,7 @@ if jq -e . >/dev/null 2>&1 <<< "$response"; then
 
   echo "Expected pre-fund: $expected_prefund"
 
-  deployer_address_balance=$(curl "$rpc_url" --location --header \
+  deployer_address_balance=$(curl -s "$rpc_url" --location --header \
   'Content-Type: application/json' --data '{
                 "jsonrpc": "2.0",
                 "method": "eth_getBalance",
@@ -168,16 +170,14 @@ if jq -e . >/dev/null 2>&1 <<< "$response"; then
     echo "Deployer address balance: $deployer_address_balance"
 
     if [ "$deployer_address_balance" -lt "$expected_prefund" ]; then
-      echo "COMMENT_OUTPUT=$(ADDRESS_NOT_PREFUNDED_ERR_MSG $expected_prefund)" >> $GITHUB_ENV
-      exit 1
+      report_error "$(ADDRESS_NOT_PREFUNDED_ERR_MSG $expected_prefund)"
     fi
   else
-    echo "COMMENT_OUTPUT=$ERROR_MSG_PREFUND_CHECK" >> $GITHUB_ENV
-    exit 1
+    report_error "$ERROR_MSG_PREFUND_CHECK"
   fi
 else
-  echo "COMMENT_OUTPUT=$ERROR_MSG_PREFUND_CHECK" >> $GITHUB_ENV
-  exit 1
+  report_error "$ERROR_MSG_PREFUND_CHECK"
 fi
 
 echo "COMMENT_OUTPUT=$SUCCESS_MSG" >> $GITHUB_ENV
+echo "LABEL_OPERATION=--add-label" >> $GITHUB_ENV
