41c77795400fb08d90882ba548418f29a3056202
Nicholas Rodrigues Lordello <nick@safe.global>
2024-05-07 12:35:00 +0200

    Introduce Utility to Deploy From GitHub Issue (#452)
    This PR adds a new `github-deploy.sh` script that can be used to deploy
    the Safe singleton factory from a GitHub issue.
    
    Sample GH PR created with script:
    https://github.com/safe-global/safe-singleton-factory/pull/454
    
    ### Test Plan
    
    While the private key is confidential, you can still test this script
    against a local network with an alternative account. For this some
    hidden magical `GITHUB_DEPLOY_...` environment variables were added for
    local testing:
    
    1. Create a new 1Password item and get its ID; you must ensure that it
    has a `password` field that is the mnemonic of the account to use for
    deployment (which must have sufficient funds).
    2. Run a local development node (tip: you can use `anvil` and the `test
    test ... test junk` mnemonic).
    3. run `GITHUB_DEPLOY_ITEMUID=... bash bin/github-deploy.sh 472`
    
    This will run the entire script **except** actually publishing the
    branch and creating the PR with the `gh` tool for testing purposes.
    
    ---------
    
    Co-authored-by: Shebin John <admin@remedcu.com>

---
+++ bin/github-deploy.sh
@@ -0,0 +1,105 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+itemuid="${GITHUB_DEPLOY_ITEMUID:-pvct2lcpjspsvgwbh2wk3riq3m}"
+
+usage() {
+    cat <<EOF
+Deploy a Safe singleton factory to a new network.
+
+This tool wraps the NPM scripts 'estimate-compile' and 'submit' and:
+1. Parses the RPC URL from a GitHub issue
+2. Fetches the deployer mnemonic from 1Password
+3. Automatically creates the GitHub PR for adding the deployment
+
+USAGE
+    github-deploy.sh [ISSUE]
+
+ARGUMENTS
+    ISSUE       The GitHub issue number of the 'new-chain' issue to deploy the
+                Safe singleton factory for. If no issue is specified, all
+                GitHub issues that are ready to deploy will be listed.
+
+EXAMPLES
+    List all issues that can be deployed:
+        github-deploy.sh
+
+    Deploy Safe singleton factory for GitHub issue #42:
+        github-deploy.sh 42
+EOF
+}
+
+if [[ -f .env ]]; then
+    echo "ERROR: Please remove '.env' file as it interferes with this script" 1>&2
+    exit 1
+fi
+if [[ -n "$(git status --porcelain)" ]]; then
+    echo "ERROR: Dirty Git index, please commit all changes before continuing" 1>&2
+    exit 1
+fi
+if ! command -v gh &> /dev/null; then
+    echo "ERROR: Please install the 'gh' GitHub CLI" 1>&2
+    exit 1
+fi
+if ! command -v op &> /dev/null; then
+    echo "ERROR: Please install the 'op' 1Password CLI" 1>&2
+    exit 1
+fi
+
+issue=0
+case $# in
+    0)
+        gh issue list --label ready-to-deploy --json number,title --jq '.[] | "• #\(.number): \(.title)"'
+        exit 0
+        ;;
+    1)
+        if ! [[ $1 =~ ^[0-9]+$ ]]; then
+            echo "ERROR: $1 is not a valid GitHub issue number" 1>&2
+            usage
+            exit 1
+        fi
+        issue=$1
+        ;;
+    *)
+        usage
+        exit 1
+        ;;
+esac
+
+echo "### Fetching RPC URL"
+rpc="$(gh issue view $issue | grep -E -o 'https?://[^ ]+' -m 1 | head -1)"
+echo "=> $rpc"
+
+echo "### Building Deployment Transaction"
+mnemonic="$(op item get "$itemuid" --field password)"
+MNEMONIC="$mnemonic" RPC="$rpc" yarn -s estimate-compile
+commit=1
+if [[ -n "$(git status --untracked-files=no --porcelain -- artifacts/)" ]]; then
+    echo "WARN: Modified an existing deployment" 1>&2
+    commit=0
+fi
+
+echo "### Submitting Transaction"
+if [[ $commit -eq 1 ]]; then
+    RPC="$rpc" yarn -s submit
+else
+    echo "WARN: Cannot automatically submit, to manually submit run:" 1>&2
+    echo "      RPC='$rpc' yarn submit" 1>&2
+fi
+
+echo "### Creating PR"
+git checkout -b "$issue-github-deployment"
+git add artifacts/
+git commit -m "$(cat <<EOF
+$(gh issue view $issue --json title --jq .title)
+
+Fixes #$issue
+EOF
+)"
+if [[ $commit -eq 1 ]]; then
+    git push --set-upstream origin "$issue-github-deployment"
+    gh pr create --fill --reviewer safe-global/safe-protocol
+else
+    echo "WARN: Cannot automatically create PR" 1>&2
+fi
--- scripts/submit.ts
+++ scripts/submit.ts
@@ -1,21 +1,42 @@
 import { promises as filesystem } from 'fs'
 import * as path from 'path'
-import { ethers } from 'ethers'
+import { ethers, BytesLike } from 'ethers'
 import dotenv from "dotenv";
 import { runScript } from './utils';
 
 dotenv.config()
 
+const rpcUrl = process.env.RPC
+const provider = new ethers.providers.JsonRpcProvider(rpcUrl)
+
+async function simulateTransaction(transaction: BytesLike) {
+	const call = ethers.utils.parseTransaction(transaction)
+	const nonce = await provider.getTransactionCount(call.from as string)
+	console.log({ nonce })
+	if (call.nonce !== nonce) {
+		throw new Error('nonce must be 0')
+	}
+	for (const property of ["hash", "type", "v", "r", "s"] as const) {
+		delete call[property]
+	}
+	console.log({ call })
+	const deployedBytecode = await provider.call(call as any)
+	console.log({ deployedBytecode })
+}
+
 async function submitDeploymentTransaction() {
-	const rpcUrl = process.env.RPC
-	const provider = new ethers.providers.JsonRpcProvider(rpcUrl)
-	const chainId = (await provider.getNetwork()).chainId
-	console.log({chainId})
+	const { chainId } = await provider.getNetwork()
+	console.log({ chainId })
 	const filePath = path.join(__dirname, "..", "artifacts", `${chainId}`, "deployment.json")
-	const deploymentData = JSON.parse(await filesystem.readFile(filePath, { encoding: 'utf8' }))
-	
-	const submittedTx = await provider.send("eth_sendRawTransaction", [deploymentData.transaction])
-	console.log("Transaction Hash", submittedTx)
+	const { transaction } = JSON.parse(await filesystem.readFile(filePath, { encoding: 'utf8' }))
+	await simulateTransaction(transaction)
+	const transactionHash = await provider.send("eth_sendRawTransaction", [transaction])
+	console.log({ transactionHash })
+	const transactionReceipt = await provider.waitForTransaction(transactionHash, 1, 60000)
+	console.log({ transactionReceipt })
+	if (transactionReceipt.status !== 1) {
+		throw new Error("deployment transaction reverted")
+	}
 }
 
 runScript(submitDeploymentTransaction)
