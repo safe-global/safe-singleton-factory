fa2a8b7c3681f3937456c4324779c493d9e3ccd3
Shebin John <admin@remedcu.com>
2025-04-09 14:50:25 +0200

    Review changes

--- scripts/new-chain.ts
+++ scripts/new-chain.ts
@@ -2,18 +2,27 @@ import fs from 'fs'
 import path from 'path'
 import { ethers } from 'ethers'
 import dotenv from "dotenv";
-import { NewChainError, runScript } from './utils';
+import { runScript } from './utils';
 import { ADDRESS, CODEHASH, FACTORY_BYTECODE, SIGNER } from './constants';
 
 dotenv.config()
 
 async function newChainWrapper() {
-	const summary: Record<string, unknown> = {};
-	summary.labelOperation = "--remove-label"
+	let summary: { commentOutput: string, labelOperation: string } = {
+		commentOutput: "An unexpected error occurred",
+		labelOperation: "--remove-label"
+	};
 	try {
-		await verifyNewChainRequest(summary)
+		await verifyNewChainRequest()
+		summary = {
+			commentOutput: `**✅ Success:**<br>The issue description is valid:<br>- The RPC URL is valid<br>- The chain is in the chainlist<br>- The deployer address is pre-funded<br>:sparkles: The team will be in touch with you soon :sparkles:`,
+			labelOperation: "--add-label"
+		}
 	} catch (error) {
-		summary.response = error
+		summary = {
+			commentOutput: (error instanceof NewChainError) ? error.comment : `Unknown Error: ${error}`,
+			labelOperation: "--remove-label"
+		}
 	} finally {
 		const summaryFile = process.env.SUMMARY_FILE
 		if (summaryFile) {
@@ -24,7 +33,7 @@ async function newChainWrapper() {
 	}
 }
 
-async function verifyNewChainRequest(summary: Record<string, unknown>) {
+async function verifyNewChainRequest() {
 	// Extract the RPC URL (first URL) from the issue body as a string
 	const rpcUrl = process.env.RPC
 	if (!rpcUrl) {
@@ -33,7 +42,7 @@ async function verifyNewChainRequest(summary: Record<string, unknown>) {
 
 	const provider = new ethers.providers.JsonRpcProvider(rpcUrl)
 	const { chainId } = await provider.getNetwork()
-	summary.chainId = chainId
+	console.log({ chainId })
 	const filePath = path.join(__dirname, "..", "artifacts", `${chainId}`, "deployment.json")
 
 	// Check if the factory is already deployed
@@ -49,8 +58,7 @@ async function verifyNewChainRequest(summary: Record<string, unknown>) {
 	// If the chain is listed, we can proceed with the deployment
 	const chainlist = `https://raw.githubusercontent.com/ethereum-lists/chains/master/_data/chains/eip155-${chainId}.json`
 	const { ok: onChainlist } = await fetch(chainlist)
-	summary.chainlist = chainlist
-	summary.onChainlist = onChainlist
+	console.log({ chainlist, onChainlist })
 	if (!onChainlist) {
 		throw NewChainError.chainNotListed(chainId.toString())
 	}
@@ -58,9 +66,7 @@ async function verifyNewChainRequest(summary: Record<string, unknown>) {
 	const nonce = await provider.getTransactionCount(SIGNER)
 	const code = await provider.getCode(ADDRESS)
 	const codehash = ethers.utils.keccak256(code)
-	summary.nonce = nonce
-	summary.codehash = codehash
-	summary.code = code
+	console.log({ nonce, codehash, code })
 	// Check if any code is deployed at the address
 	if (ethers.utils.hexDataLength(code) > 0) {
 		// Check if the codehash matches the expected codehash
@@ -96,9 +102,7 @@ async function verifyNewChainRequest(summary: Record<string, unknown>) {
 		}
 
 		const gasEstimate = gasPrice.mul(gasLimit!).mul(15).div(10) // 15% buffer
-		summary.gasPrice = gasPrice.toString()
-		summary.gasLimit = gasLimit?.toString()
-		summary.gasEstimate = gasEstimate.toString()
+		console.log({ gasPrice: gasPrice.toString(), gasLimit: gasLimit?.toString(), gasEstimate: gasEstimate.toString() })
 
 		// Get the deployed bytecode simulation
 		let simulation: string = '';
@@ -110,9 +114,9 @@ async function verifyNewChainRequest(summary: Record<string, unknown>) {
 		} catch (error) {
 			throw NewChainError.deploymentSimulationFailed()
 		}
-		summary.simulation = simulation
+		console.log({ simulation })
 		const simulationCodehash = ethers.utils.keccak256(simulation)
-		summary.simulationCodehash = simulationCodehash
+		console.log({ simulationCodehash })
 		// Check if the simulation codehash matches the expected codehash
 		if (simulationCodehash !== CODEHASH) {
 			throw NewChainError.factoryDeploymentSimulationDifferentBytecode()
@@ -120,13 +124,111 @@ async function verifyNewChainRequest(summary: Record<string, unknown>) {
 
 		// Check if the deployer account has enough balance
 		const balance = await provider.getBalance(SIGNER)
-		summary.balance = ethers.utils.formatEther(balance)
+		console.log({ balance: ethers.utils.formatEther(balance) })
 		if (balance.lt(gasEstimate)) {
 			throw NewChainError.prefundNeeded(gasEstimate.toString(), SIGNER)
 		}
 	}
-	summary.response = `**✅ Success:**<br>The issue description is valid:<br>- The RPC URL is valid<br>- The chain is in the chainlist<br>- The deployer address is pre-funded<br>:sparkles: The team will be in touch with you soon :sparkles:`
-	summary.labelOperation = "--add-label"
+}
+
+class NewChainError extends Error {
+	public comment: string;
+	private constructor(message: string, comment: string) {
+		super(message);
+		this.name = "NewChainError";
+		this.comment = comment;
+	}
+	
+	static rpcNotFound() {
+		return new NewChainError(
+			"RPC URL not found",
+			`**⛔️ Error:**<br>RPC URL not found in the issue body.`
+		);
+	}
+
+	static factoryAlreadyDeployed() {
+		return new NewChainError(
+			"Factory already deployed",
+			`**⛔️ Error:**<br>The factory is already deployed.`
+		);
+	}
+
+	static chainNotListed(chainId: string) {
+		return new NewChainError(
+			"Chain not listed",
+			`**⛔️ Error:**<br>Chain ${chainId} is not listed in the chainlist. For more information on how to add a chain, please refer to the [chainlist repository](https://github.com/ethereum-lists/chains).<br>`
+		);
+	}
+
+	static factoryDifferentBytecode() {
+		return new NewChainError(
+			"Factory different bytecode",
+			`**⛔️ Error:**<br>Factory is deployed with different bytecode.`
+		);
+	}
+
+	static factoryPreDeployed() {
+		return new NewChainError(
+			"Factory pre-deployed",
+			`**⛔️ Error:**<br>Factory is pre-deployed on the chain.`
+		);
+	}
+
+	static factoryNotAddedToRepo() {
+		return new NewChainError(
+			"Factory not added to repo",
+			`**⛔️ Error:**<br>Factory has been deployed but not added to the repository.`
+		);
+	}
+
+	static factoryDeployerAccountNonceBurned() {
+		return new NewChainError(
+			"Factory deployer account nonce burned",
+			`**⛔️ Error:**<br>Factory deployer account nonce burned.`
+		);
+	}
+
+	static gasPriceNotRetrieved() {
+		return new NewChainError(
+			"Gas price not retrieved",
+			`**⛔️ Error:**<br>Gas price couldn't be retrieved. Please make sure that the RPC URL is valid and reachable.`
+		);
+	}
+
+	static gasLimitNotEstimated() {
+		return new NewChainError(
+			"Gas limit not estimated",
+			`**⛔️ Error:**<br>Gas limit couldn't be estimated. Please make sure that the RPC URL is valid and reachable.`
+		);
+	}
+
+	static gasLimitEstimationFailed() {
+		return new NewChainError(
+			"Gas limit estimation failed",
+			`**⛔️ Error:**<br>Gas limit estimation failed. Please make sure that the RPC URL is valid and reachable.`
+		);
+	}
+
+	static deploymentSimulationFailed() {
+		return new NewChainError(
+			"Deployment simulation failed",
+			`**⛔️ Error:**<br>Deployment simulation failed. Please make sure that the RPC URL is valid and reachable.`
+		);
+	}
+
+	static factoryDeploymentSimulationDifferentBytecode() {
+		return new NewChainError(
+			"Factory deployment simulation different bytecode",
+			`**⛔️ Error:**<br>Factory deployment simulation returned different bytecode.`
+		);
+	}
+
+	static prefundNeeded(amount: string, signer: string) {
+		return new NewChainError(
+			"Prefund needed",
+			`**💸 Pre-fund needed:**<br/>We need a pre-fund to deploy the factory. Please send ${amount} wei to ${signer} and check the checkbox in the issue.`
+		);
+	}
 }
 
 runScript(newChainWrapper)
